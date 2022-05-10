ff940c8defa4e4044e5c5505cf46080c937adf51
Tatiana2512 <hikari_neko@mail.ru>
2022-05-10 23:47:13 +0400

    add attachments add step annotation

--- src/test/java/ui/RegistrationFormTest.java
+++ src/test/java/ui/RegistrationFormTest.java
@@ -3,10 +3,17 @@ package ui;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import data.TestDataFaker;
+import helper.Attachments;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import pages.RegistrationFormPage;
+
+import static com.codeborne.selenide.Selenide.closeWebDriver;
+import static io.qameta.allure.Allure.step;
 
 public class RegistrationFormTest {
 
@@ -15,15 +22,41 @@ public class RegistrationFormTest {
         //Configuration.holdBrowserOpen = true;
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "1400x800";
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", true);
+        Configuration.browserCapabilities = capabilities;
     }
 
     @Test
+    @DisplayName("Check Registration Form")
     public void fillTheFormTest() {
         SelenideLogger.addListener("allure", new AllureSelenide());
         TestDataFaker student = new TestDataFaker();
         RegistrationFormPage page = new RegistrationFormPage();
-        page.openPage().fillTheForm(student).checkTheForm(student);
+        step("Open registration form", () -> {
+            page.openPage();
+        });
 
+        step("Fill registration form", () -> {
+            page.fillTheForm(student);
+        });
+
+        step("Verify accepted data", () ->{
+            page.checkTheForm(student);
+        });
+
+    }
+
+    @AfterEach
+    void addAttachments() {
+        Attachments.screenshotAs("Last screenshot");
+        Attachments.pageSource();
+        Attachments.browserConsoleLogs();
+        Attachments.addVideo();
+        closeWebDriver();
     }
 
 }
